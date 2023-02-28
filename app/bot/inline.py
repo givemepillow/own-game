799cb2e3bd1644dd43eb01982f0223e2b2077405
Kirill Lapushinskiy <kirill@lapushinskiy.ru>
2023-02-28 11:23:23 +0300

    feat: классы-данные для встроенных клавиатур

---
+++ app/bot/inline.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Iterable, Final
+
+PLUG: Final = '_'
+
+
+@dataclass(slots=True)
+class CallbackData:
+    type: str = PLUG
+    value: str = PLUG
+
+
+@dataclass(slots=True)
+class InlineButton:
+    text: str = ' '
+    callback_data: CallbackData = CallbackData()
+
+
+class InlineKeyboard:
+    def __init__(self):
+        self._keyboard = []
+
+    def __iter__(self) -> Iterable[tuple[InlineButton]]:
+        return iter(self._keyboard)
+
+    def add(self, *buttons: InlineButton):
+        self._keyboard.append(buttons)
