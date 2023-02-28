3a0961d4bcff370d4d4c1e50b0aab7c0cb815699
Kirill Lapushinskiy <kirill@lapushinskiy.ru>
2023-02-28 19:28:34 +0300

    feat: класс шины сообщений

--- app/store/__init__.py
+++ app/store/__init__.py
@@ -1,3 +1,4 @@
+from app.store.bus import MessageBus
 from app.store.database import Database
 
 from app.web.application import Application
@@ -10,3 +11,4 @@ class Store:
 
 def setup_store(app: Application):
     app['store'] = Store(app)
+    app['bus'] = MessageBus(app)
